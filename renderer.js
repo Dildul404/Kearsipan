f4c4a861e6fb28040ebe0420f64596bf06919cf2
unknown <justenjoycode09@gmail.com>
2025-10-24 19:50:56 +0700

    menambah beberapa fitur

--- renderer.js
+++ renderer.js
@@ -1,22 +1,59 @@
 // fetch 
 function fetching(link) {
-    fetch(link)
+  fetch(link)
     .then(response => response.text())
     .then(data => {
-        document.getElementById("contentContainer").innerHTML = data;
-    });
+      document.getElementById("contentContainer").innerHTML = data;
+
+      // Pasang listener setelah konten baru masuk
+      // memasukkan surat ke halaman edit
+      const editBtn = document.getElementById("BtnEdit");
+      if (editBtn) {
+          editBtn.addEventListener("click", async () => {
+
+            // muat halaman edit
+            const editRes = await fetch("edit.html");
+            const editHtml = await editRes.text();
+            document.getElementById("contentContainer").innerHTML = editHtml;
+
+            // baru setelah itu, muat surat
+            const suratRes = await fetch("surat-surat/surat1.html");
+            const suratHtml = await suratRes.text();
+            document.getElementById("pembungkusSurat").innerHTML = suratHtml;
+          });
+        }
+
+      // Batal btn
+      const batalBtn = document.getElementById("BtnBatal");
+      const sectionDua = document.getElementById("dua");
+
+      if(batalBtn) {
+        if(sectionDua) {
+          batalBtn.addEventListener("click", (e)=> {
+            sectionDua.classList.add("hidden");
+          });
+        }
+      }
+
+      // template surat
+      let surat
+      document.querySelectorAll("#templateItem").forEach(surat => {
+        surat.addEventListener("click", e => {
+          e.preventDefault();
+          sectionDua.classList.remove("hidden");
+        });
+      });
+
+
+    }); 
 }
 
-// main View 
-// fetching("mainView.html");
-
-// change content with a tag
+// berpindah halaman (halaman utama)
 let url
 document.querySelectorAll("#linkMainView").forEach(link => {
   link.addEventListener("click", e => {
     e.preventDefault();
     url = link.getAttribute('href');
     fetching(url);
-    // return url;
   });
 });
